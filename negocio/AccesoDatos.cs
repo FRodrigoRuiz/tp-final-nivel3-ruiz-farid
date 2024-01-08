f29244685346314f84b92be834a465da50c78264
RodrigoRuiz <farid.ruiz2000@gmail.com>
2024-01-08 00:57:43 -0300

    connection configuration added

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace negocio
 
         public AccesoDatos()
         {
-            conexion = new SqlConnection("server=DESKTOP-IAERP4O\\SQLAS2022; database=CATALOGO_WEB_DB; integrated security=true");
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["cadenaConexion"]);
             comando = new SqlCommand();
         }
 
